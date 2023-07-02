a766be1b98a3acc6d7233fd8aa102321e72ab614
John Santosuosso <john.santosuosso1@gmail.com>
2023-07-02 17:16:47 -0400

    add faraday gem add stock search page with search box fix footer to always center on page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+
+  get "/search", to: 'search#index'
+
+  get "/symbol-lookup", to: 'symbol_lookup#index'
+  get "/symbol-lookup/search", to: 'symbol_lookup#search', as: 'symbol_lookup_results' 
+
 end
